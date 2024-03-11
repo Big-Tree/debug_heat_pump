ed5b9e2d25c6222e3a10bbbbd4283e65ceeb2d0b
William Murphy <william.murphy333@gmail.com>
2024-03-11 11:36:07 +0000

    Fixed hacs releases

--- custom_components/debug_heat_pump/const.py
+++ custom_components/debug_heat_pump/const.py
@@ -5,7 +5,7 @@ LOGGER: Logger = getLogger(__package__)
 
 NAME = "Debug Heat Pump"
 DOMAIN = "debug_heat_pump"
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 ATTRIBUTION = "Attribution"
 
 MODE = "debug_heat_pump_mode"
